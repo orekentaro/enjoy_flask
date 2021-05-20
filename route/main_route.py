67eedbc1dab7ca714747aad25b0cd87df5311376
orekentaro <kntru0218gj@gmail.com>
2021-05-20 23:25:15 +0900

    ログイン処理

--- route/main_route.py
+++ route/main_route.py
@@ -6,6 +6,20 @@ main_route = Blueprint('main_route', __name__, url_prefix='/')
 
 
 MainModel = MainModel()
+
 @main_route.route('/login')
 def login():
+  """
+  初期表示
+  ログイン画面
+  """
   return MainModel.login()
+
+@main_route.route('/login_check', methods=['POST'])
+def login_check():
+  """
+  初期表示
+  ログイン画面
+  """
+  return MainModel.login_check()
+
